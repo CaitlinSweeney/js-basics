419cf4049cd8cac0d61aa83b2512ef4613b9c63f
Student <student@Admins-Mac-mini-8.local>
2016-07-01 15:14:12 -0600

    js-basics

--- script.js
+++ script.js
@@ -1,64 +1,6 @@
-//JAVASCRIPT BASICS
-
-var a =1,
-var b = 2
-
-console.log(a-b);
-consol.log(12&5);
-
-//Strings
-
-'a'
-'1' //this is not a number, this is a string
-
-'hello'
-'man donuts sound good right now..'
-'don/t do this' //this '/' is the exacpe character in JS
-var myString = "instead, use double quotes!"
-
-//String Operators
-// +
-
-var firstWord= 'Hello'
-var secondWord= 'from the ouuuutside'
-firstWord + '' + secondWord
-
-// firstWord. < the dot is called an 'accessor'
-//find out the lenght of an expression
-//(firstWord + '' + secondPhrase).length
-
-firstWord[0] // bracket notation
-
-//booleans
-//true
-//false
-//! not
-//!= not equal to 
-//== equilvalent to
-//== absolute, literally equal to
-// && and
-// || or
-//  < less than <= less than equal too
-// > greater than >= greater than or equal to
-
-var a = 2
-var b = '2'
-var c = 3
-var d = 0
-
-a != b //fuzzy... returns "false"
-a !== b // returns "true"
-
-//CONDITIONAL STATEMENTS or "if/else statement"
-
-if(true) { //what happens here will execute if true
-	console.log("this will happen!")
-}else{//false return will preform}
-
-
-var myRecipe = "Pie"
-var myIngredients = "egg, flour, baking soda, vanilla, coconut, sugar"
-
+function hello(){
+	alert("Hello Boulder!");
+}
 
 
 

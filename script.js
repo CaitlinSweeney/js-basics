23f0f3f69f3495df84d4fada93aa6667a1a13bd1
Student <student@Admins-Mac-mini-8.local>
2016-07-01 16:20:42 -0600

    update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function hello(){
-	alert("Hello Boulder!");
+// function hello(){
+// 	alert("Hello Boulder!");
+// }
+
+
+function yourName(){
+	var bananas = prompt("What's Your Name", "SAY YOUR NAME!")
+	if (bananas != null) {
+		document.getElementById("apples").innerHTML = "Hello" + " " + bananas + "!";
+	}
 }
-
-
-
-
+	
+// yourName();

e705ea6760d9afa030af8a9473e238efc5a9df49
Caitlin Sweeney <caitlin.marie.sweeney@gmail.com>
2016-07-04 16:56:39 -0600

    Add files via upload

---
+++ strings/script.js
@@ -0,0 +1,20 @@
+//String Reporter
+
+var feels = window.prompt("Are you haveing a fab day?");
+  if (feels == "yes" || feels == "yeah" || feels == "yup" || feels == "sure") {
+    alert("sweeeeet");
+  } else { alert("let's get happy!");
+  }
+
+function myFruits()
+{
+var apples=prompt("Do you eat alot of fruits?", "Yes or No");
+if (apples!=null && apples=="Yes")
+  {
+  alert("Great! You're a healthy human!");
+  }
+else
+  {
+  alert("But I love Apples and Bananas! You need more vitamins:)");
+  }
+}
